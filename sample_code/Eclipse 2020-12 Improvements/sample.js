db5de3fcc6e52240afc6727329ed151879757fb9
Holger Voormann <github@voormann.de>
2020-12-13 15:57:20 +0100

    Eclipse 2020-12: Update sample code

--- sample_code/Eclipse 2020-12 Improvements/sample.js
+++ sample_code/Eclipse 2020-12 Improvements/sample.js
@@ -1,8 +1,7 @@
 function factorial(n) {
-    if (n === 0)
-        return 1; // 0! = 1
-
-    return n * factorial(n - 1);
+   if (n === 0)
+ return 1; // 0! = 1
+ return n * factorial(n - 1);
 }
 
-factorial(3); // returns 6
+  factorial(3); // returns 6
